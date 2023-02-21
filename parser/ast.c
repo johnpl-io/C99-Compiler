d8cd25a2e47d94708a6cb399bee6d16a38e4fdf5
John Pluchino <john.nycjets@gmail.com>
2023-02-20 21:02:28 -0500

    test

--- parser/ast.c
+++ parser/ast.c
@@ -1,19 +1,97 @@
-struct *astnode newNum(int nodetype, int num) {
-    int nodetype = nodetype;
-    struct *numast malloc(sizeof(struct astnode));
+#include <stdio.h>
+#include "ast.h"
+#include <stdlib.h>
+struct astnode *newNum(int nodetype, int num) {
+  
+    struct astnode *numast = malloc(sizeof(struct astnode));
+    numast->nodetype = nodetype;
     numast->num.number = num;
     numast->num.nodetype = nodetype;
     return numast;
-};
+}
 
-struct *astnode newIdent(int nodetype, char *ident);
-struct *astnode newString(int nodetype, char *string);
-primary-expresion : ident 
-                    primary-expression '+' primary-expression { $$ = newast(prim, $1, $3); $$->operator = '+';}
-                    a+b 
-                    a->ident + b->ident node
+struct astnode *newIdent(int nodetype, char *ident) { 
+    struct astnode *identast = malloc(sizeof(struct astnode));
+       identast->nodetype = nodetype;
+    identast->ident.string = ident;
+    identast->ident.nodetype = nodetype;
+    return identast;
+}
 
 
-struct *astnode newast(int nodetype, struct *astnode l, struct *astnode *r) {
-    
-};
+
+ struct astnode *newast(int nodetype, struct astnode *l, struct astnode *r) {
+    struct astnode *a = malloc(sizeof(struct astnode));
+    a->nodetype = nodetype;
+
+    switch (nodetype) {
+        case AST_NODE_TYPE_BINOP:
+            a->binop.left = l;
+            a->binop.right = r;
+            break;
+    case AST_NODE_TYPE_TENOP:
+            a->tenop.left = l;
+            a->tenop.right = r;
+            break;
+
+        case AST_NODE_TYPE_UNOP:
+            a->unop.left = l;
+            a->unop.right = r;
+            break;
+
+
+
+        // add more cases as needed for other node types
+
+        default:
+            fprintf(stderr, "Invalid node type %d\n", nodetype);
+            exit(1);
+    }
+
+return a;
+
+
+}
+static void astwalk_impl(struct astnode *ast, int depth) {
+    if (!ast) {
+        return;
+    }
+    for (int i = 0; i < depth; i++) {
+        printf("  ");
+    }
+    switch (ast->nodetype) {
+        case AST_NODE_TYPE_BINOP:
+            printf("BINOP %c\n", ast->binop.operator);
+            astwalk_impl(ast->binop.left, depth + 1);
+            astwalk_impl(ast->binop.right, depth + 1);
+            break;
+        case AST_NODE_TYPE_TENOP:
+            printf("TENOP %c\n", ast->tenop.operator);
+            astwalk_impl(ast->tenop.left, depth + 1);
+            astwalk_impl(ast->tenop.right, depth + 1);
+            break;
+        case AST_NODE_TYPE_UNOP:
+            printf("UNOP %c\n", ast->unop.operator);
+            astwalk_impl(ast->unop.left, depth + 1);
+            astwalk_impl(ast->unop.right, depth + 1);
+            break;
+        case AST_NODE_TYPE_NUM:
+            printf("NUM %d\n", ast->num.number);
+            break;
+        case AST_NODE_TYPE_IDENT:
+            printf("IDENT %s\n", ast->ident.string);
+            break;
+        default:
+            printf("Unknown node type\n");
+            break;
+    }
+}
+
+
+int main() {
+    struct astnode *a = newNum( AST_NODE_TYPE_NUM,2);
+    struct astnode *b = newNum( AST_NODE_TYPE_NUM,1);
+    struct astnode *plus = newast(AST_NODE_TYPE_BINOP, a, b);
+    plus->binop.operator = '+';
+   astwalk_impl(plus, 0);
+}
--- parser/ast.h
+++ parser/ast.h
@@ -1,48 +1,57 @@
+enum AstNodeType {
+    AST_NODE_TYPE_BINOP,
+    AST_NODE_TYPE_TENOP,
+    AST_NODE_TYPE_UNOP,
+    AST_NODE_TYPE_NUM,
+    AST_NODE_TYPE_IDENT,
+    // add more types as needed
+};
+
+struct astnode_unop {
+    int nodetype;
+    int operator;
+    struct astnode *left, *right;
+};
+
+struct astnode_binop {
+    int nodetype;
+    int operator;
+    struct astnode *left, *right;
+};
+
+struct astnode_tenop {
+    int nodetype;
+    int operator;
+    struct astnode *left, *right;
+};
+
+struct astnode_num {
+    int nodetype;
+    int number;
+    struct astnode *left, *right;
+};
+
+struct astnode_ident {
+    int nodetype;
+    char *string;
+    struct astnode *left, *right;
+};
+
 struct astnode {
 int nodetype;
 union {
     struct astnode_binop binop;
     struct astnode_tenop tenop;
     struct astnode_unop unop;
-    struct struct_num   num;
+    struct astnode_num   num;
     struct astnode_ident ident;
-    }   
-}
-
-struct astnode_unop {
-    int nodetype;
-    int operator;
-    struct astnode *left, *right;
-}
-
-struct astnode_binop {
-    int nodetype;
-    int operator;
-    struct astnode *left, *right;
-}
-
-struct astnode_tenop {
-    int nodetype;
-    int operator;
-    struct astnode *left, *right;
-}
-
-struct astnode_num {
-    int nodetype;
-    int number;
-    struct astnode *left, *right;
-}
-
-struct astnode_ident {
-    int nodetype;
-    char *string;
-    struct astnode *left, *right;
-}
+    };  
+};
 
 
-struct *astnode newast(int nodetype, struct *astnode l, struct *ast *r);
-struct *astnode newNum(int nodetype, int num);
-struct *astnode newIdent(int nodetype, char *ident);
-struct *astnode newString(int nodetype, char *string);
-void astwalk(struct *astnode);
+
+struct astnode *newast(int nodetype, struct astnode *l, struct astnode *r);
+struct astnode *newNum(int nodetype, int num);
+struct astnode *newIdent(int nodetype, char *ident);
+
 

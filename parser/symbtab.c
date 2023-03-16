83c6144e72ef0c4c4a658db012f83a30473a4965
colinh09 <colin.hwang@cooper.edu>
2023-03-15 21:35:22 -0400

    added symbol table functions

---
+++ parser/symbtab.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "symbtab.h"
+#include "parser.tab.h"
+
+// initialize new symbol table
+struct symbtab *symbtab_init(int scope) {
+    struct symbtab *table = calloc(1, sizeof(struct symbtab));
+    table->scope = scope;
+    return table;
+}
+
+// free memory used by symbol table
+void symbtab_destroy(struct symbtab *table) {
+    struct symbol *cur = table->head;
+    while (cur) {
+        struct symbol *next = cur->next;
+        free(cur->ident);
+        free(cur->filename);
+        free(cur);
+        cur = next;
+    }
+    free(table);
+}
+
+// traverse through stack of scopes and each symbol in each scope for a match
+struct symbol *symbtab_lookup(struct symbtab *table, struct symbol *symbol) {
+    struct symbtab *cur_table = table;
+    struct symbol *cur_sym = NULL;
+    while (cur_table) {
+        cur_sym = cur_table->head;
+        // check matching symbol name and namespace
+        while (cur_sym) {
+            if (strcmp(cur_sym->name, symbol->name) == 0 && cur_sym->namespace == symbol->namespace) {
+                return cur_sym;
+            }
+            cur_sym = cur_sym->next;
+        }
+        cur_table = cur_table->next;
+    }
+    return NULL;
+}
+
+// return true if installed successfully, return false if it's already there and we don't want to replace
+bool symbtab_insert(struct symbtab *table, struct symbol *symbol, bool replace) {
+    // check if symb exists within the table
+    struct symbol *exists = symbtab_lookup(table, symbol);
+    if (exists) {
+        if (replace) {
+            // replace the definition of the symbol
+            symbol->next = exists->next;
+            *exists = *symbol;
+        } else {
+            // don't replace the definition of the symbol
+            return false;
+        }
+    } else {
+        // symbol doesnt exist within table, install it
+        // placing the new symbol at the head will help with future lookups supposedly
+        symbol->next = table->head;
+        table->head = symbol;
+    }
+    return true;
+}
+
+// initialize new table and push onto stack
+struct symbtab *symbtab_push(int scope, struct symbtab *prev_symbtab) {
+    struct symbtab *new_table = symbtab_init(scope);
+    new_table->next = prev_symbtab;
+    return new_table;
+}
+
+// change the current scope (new_symbtab already initialized)
+struct symbtab *symbtab_insert_on(struct symbtab *current_symbtab, struct symbtab *new_symbtab) {
+    new_symbtab->current_symbtab = current_symbtab;
+    return new_symbtab;
+}
+
+// pop current scope, free it, and prev scope becomes new current scope
+struct symbtab *symbtab_pop(struct symbtab *current_scope) {
+    struct symbtab *prev_scope = current_scope->next;
+    symbtab_destroy(current_scope);
+    return prev_scope;
+}
---
+++ parser/symbtab.h
@@ -0,0 +1,52 @@
+enum SCOPE {
+    SCOPE_GLOBAL,
+    SCOPE_FUNCTION,
+    SCOPE_BLOCK,
+    SCOPE_PROTOTYPE,
+    SCOPE_STRUCT_UNION
+};
+
+enum NAMESPACE {
+    NAMESPACE_TAG,
+    NAMESPACE_LABEL,
+    NAMESPACE_MEMBER,
+    NAMESPACE_ALT,
+};
+
+struct symbol {
+    char *name;
+    int namespace;
+    char *filename;
+    int lineno;
+    struct astnode *astnode;
+    struct symbol *next;
+};
+
+struct symbtab {
+    struct symbol *head;
+    struct symbtab *next;
+    int scope;
+};
+
+// Create empty symbol table
+struct symbtab *symbtab_init(int scope);
+
+// Destroy: destroy a symbol table including any storage it consumes
+void symbtab_destroy(struct symbtab *table);
+
+// Lookup: given an existing symbol table, a name, and a namespace class
+// Return the associated symbol table entry, or return nothing if the symbol DNE
+struct symbol *symbtab_lookup(struct symbtab *table, struct symbol *symbol);
+
+// Enter: give an existing symbol table, a name, a namespace class, and a set of attributes
+// Insert a symbol into the symbol table
+bool symbtab_insert(struct symbtab *table, struct symbol *symbol, bool replace);
+
+// Push onto symbtable struct and return a pointer to the new symbol table
+struct symbtab *symbtab_push(int scope, struct symbtab *prev_symbtab);
+
+// Make an existing symbol table the current scope we're working with
+struct symbtab *symbtab_insert_on(struct symbtab *current_symbtab, struct symbtab *insert_symbtab);
+
+// Pop from symbtable struct go the previous scope
+struct symbtab *symbtab_pop(struct symbtab *current_scope);
